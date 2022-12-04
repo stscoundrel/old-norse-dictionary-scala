e44730ee7e054825db8ec22222f67adf21792250
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-12-04 15:53:41 +0200

    Swap dummy src from Java to Scala

---
+++ src/main/scala/io/github/stscoundrel/oldnorsedictionary/App.scala
@@ -0,0 +1,11 @@
+package io.github.stscoundrel.oldnorsedictionary
+
+object App {
+  def main(args: Array[String]): Unit = {
+    println("Hello World!")
+  }
+
+  def dummyMethod(first: Int, second: Int): Int = {
+    first + second
+  }
+}
