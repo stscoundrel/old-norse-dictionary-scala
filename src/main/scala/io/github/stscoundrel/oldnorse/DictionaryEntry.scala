a7c9b7f3c2494382efbe2acf53944fed11c905df
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-07-10 17:56:18 +0300

    Use lazy vals for entries, reformat all code
    Closes #36

--- src/main/scala/io/github/stscoundrel/oldnorse/DictionaryEntry.scala
+++ src/main/scala/io/github/stscoundrel/oldnorse/DictionaryEntry.scala
@@ -3,6 +3,6 @@ package io.github.stscoundrel.oldnorse
 import com.google.gson.annotations.SerializedName
 
 case class DictionaryEntry(
-  @(SerializedName @scala.annotation.meta.field)("a") val headword: String,
-  @(SerializedName @scala.annotation.meta.field)("b") val definitions: Array[String]
-)
+                            @(SerializedName@scala.annotation.meta.field)("a") val headword: String,
+                            @(SerializedName@scala.annotation.meta.field)("b") val definitions: Array[String]
+                          )
