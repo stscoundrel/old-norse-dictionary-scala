083dcd19924400b6cf6e615391cc1b6d6fb29cca
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-07-10 20:42:25 +0300

    DictionaryEntry: use more readable annotations

--- src/main/scala/io/github/stscoundrel/oldnorse/DictionaryEntry.scala
+++ src/main/scala/io/github/stscoundrel/oldnorse/DictionaryEntry.scala
@@ -1,8 +1,12 @@
 package io.github.stscoundrel.oldnorse
 
 import com.google.gson.annotations.SerializedName
+import scala.annotation.meta.field
 
 case class DictionaryEntry(
-                            @(SerializedName@scala.annotation.meta.field)("a") val headword: String,
-                            @(SerializedName@scala.annotation.meta.field)("b") val definitions: Array[String]
-                          )
+  @(SerializedName@field)("a")
+  val headword: String,
+
+  @(SerializedName@field)("b")
+  val definitions: Array[String]
+)
